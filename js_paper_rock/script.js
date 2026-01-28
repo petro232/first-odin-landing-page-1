aa89566f60ca60bbfce5f970061b04579abf73d5
pet_faozui <faouzikaramazo155@gmail.com>
2026-01-28 14:57:03 +0000

    finish buttons working

--- js_paper_rock/script.js
+++ js_paper_rock/script.js
@@ -1,4 +1,7 @@
 let choice=["rock","paper","scissors"]
+ let computer_scoor = 0
+let human_scoor = 0
+
 
 function getcomputerchoice(grap){
     let rand = Math.floor(Math.random() * 3  )
@@ -13,39 +16,69 @@ function getHumanChoice(){
         return human_pick.toLowerCase()
     }
 }
-
-function playGame(){
-    let computer_scoor = 0
-    let human_scoor = 0
-
+ 
     function playRound(humanChoice, computerChoice){
     console.log(`You picked: ${humanChoice}, Computer picked: ${computerChoice}`)
     if (humanChoice === computerChoice){
-        console.log("draw round")
+        
+        document.querySelector("#results").textContent = "draw round"
+        human_scoor += 1
+        computer_scoor += 1
+        document.querySelector("#score").textContent = `\nhumanscore ${human_scoor} : computerscore : ${computer_scoor}`
     }
     else if (humanChoice === "rock" && computerChoice === "scissors" || humanChoice === "paper" && computerChoice === "rock" || humanChoice === "scissors" && computerChoice === "paper"){
-        console.log("human won")
+        // console.log(` ${humanChoice} beats  ${computerChoice}`)
+        document.querySelector("#results").textContent = ` ${humanChoice} beats  ${computerChoice}`
         human_scoor +=1
+        document.querySelector("#score").textContent = `\nhumanscore ${human_scoor} : computerscore : ${computer_scoor}`
     }
     else if (humanChoice === "rock" && computerChoice == "paper" || humanChoice === "paper" && computerChoice === "scissors" || humanChoice === "scissors" && computerChoice === "rock"){
-        console.log("computer won")
+        // console.log(`  ${humanChoice} loses to  ${computerChoice}`)
+        document.querySelector("#results").textContent = `${humanChoice} loses to  ${computerChoice}`
         computer_scoor += 1
+        document.querySelector("#score").textContent = `\nhumanscore ${human_scoor} : computerscore : ${computer_scoor}`
+
+        if (human_scoor === 5) {
+        document.querySelector("#score").textContent += "\nHuman wins the game!"
+        btn_rock.disabled = true
+        btn_paper.disabled = true
+        btn_sissors.disabled = true
+}
+        else if (computer_scoor === 5) {
+        document.querySelector("#score").textContent += "\nComputer wins the game!"
+        btn_rock.disabled = true
+        btn_paper.disabled = true
+        btn_sissors.disabled = true
+}
     }   
 }
-    for ( let i=0 ; i < 5 ; i++){
-        let human_pick =getHumanChoice()
-        let computer_pick =getcomputerchoice(choice)
-        playRound(human_pick,computer_pick)
-    } if (human_scoor > computer_scoor){
-        console.log(`human won by: ${human_scoor} to ${computer_scoor} `)
-    }
-    else if (human_scoor === computer_scoor){
-        console.log(`draw: human : ${human_scoor} to  computer: ${computer_scoor} `)
-    }
-    else {
-        console.log(`computer won by: ${computer_scoor} to ${human_scoor} `)
-    }
 
-}
 
- playGame();
+
+
+
+
+ 
+ 
+let btn_rock = document.querySelector("#rock")
+let btn_paper = document.querySelector("#paper")
+let btn_sissors = document.querySelector("#scissors")
+let computer_pick = getcomputerchoice (choice)
+ 
+btn_rock.addEventListener("click", function(){
+    let computer_pick = getcomputerchoice(choice)
+    playRound("rock", computer_pick)
+})
+
+btn_paper.addEventListener("click", function(){
+    let computer_pick = getcomputerchoice(choice)
+    playRound("paper", computer_pick)
+})
+
+btn_sissors.addEventListener("click", function(){
+    let computer_pick = getcomputerchoice(choice)
+    playRound("scissors", computer_pick)
+})
+
+ 
+ 
